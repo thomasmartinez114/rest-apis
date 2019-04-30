b4c31b0d218daca5147ebb4caa6683a32bd3b469
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-04-30 05:17:56 +0000

    completed tutorial05

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 // set up express app
 const app = express();
 
+app.get('/api', (req, res) => {
+    console.log('GET request');
+    res.send({name: 'Yoshi'});
+})
+
 // listen for requests
 app.listen(process.env.PORT || 4000, function(){
    console.log('now listening for requests');
