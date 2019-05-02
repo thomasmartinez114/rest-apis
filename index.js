fccc03a51e1e47e9c52b26f004b59602c81b1ff0
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-02 18:37:51 +0000

    saving data to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // set up express app
 const app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb:./mongod');
+mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 
